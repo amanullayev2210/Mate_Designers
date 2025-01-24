bd8a183ceaaebf1712401ba7dea106128b4c007c
amanullayev2210 <karory577@gmail.com>
2025-01-25 00:35:51 +0500

    Update js

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+const menu = document.querySelector(".menu");
+const closes = document.querySelector(".close");
+
+let data = true;
+
 document.querySelector(".js-btn").addEventListener("click", (evt) => {
   evt.preventDefault();
-
   document
     .querySelector(".js-btn")
     .closest(".site-header")
     .classList.toggle("open");
+
+  if (data) {
+    menu.style.display = "none";
+    closes.style.display = "block";
+    data = false;
+  } else {
+    menu.style.display = "block";
+    closes.style.display = "none";
+    data = true;
+  }
 });
